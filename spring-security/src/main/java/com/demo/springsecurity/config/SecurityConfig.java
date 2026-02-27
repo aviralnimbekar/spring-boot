89166f2a94f635ff91534a57d94c4333f59856ba
Aviral Nimekar <aviralnimekar12@gmail.com>
2026-02-27 10:37:51 +0530

    Add: InMemory user details provider implementation for in memory auth

--- spring-security/src/main/java/com/demo/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/demo/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.demo.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,6 +20,22 @@ public class SecurityConfig {
 //        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+//                used {noop} to store password as plain text, otherwise use password encoder
+                .password("{noop}userpass")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}adminpass")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
