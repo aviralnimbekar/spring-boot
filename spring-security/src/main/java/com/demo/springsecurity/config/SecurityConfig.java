1768710078d51299b8c9f551d8494ae1147f4eb4
Aviral Nimekar <aviralnimekar12@gmail.com>
2026-03-02 11:00:12 +0530

    Add: Add pg db docker image and flyway

--- spring-security/src/main/java/com/demo/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/demo/springsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity // If you don't use this annotation Sprint-boot will add this internally
 @EnableMethodSecurity
 public class SecurityConfig {
 
